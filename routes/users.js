513b0d05f3502cebf93b30a56770f88f59990316
Eduardo Sousa <dudumarques.510@gmail.com>
2025-09-21 15:10:44 -0300

    refactor: ajuste na rota de usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/userController')
 
-// --- Registro e Login ---
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
+router.post('/register', userController.registerUser)
+router.post('/login', userController.loginUser)
 
-// --- CRUD completo ---
-router.get('/users', userController.getUsers);           // Listar todos
-router.get('/users/:id', userController.getUserById);    // Buscar por ID
-router.post('/users', userController.createUser);       // Criar usuário
-router.put('/users/:id', userController.updateUser);    // Atualizar usuário
-router.delete('/users:id', userController.deleteUser); // Deletar usuário
+router.get('/', userController.getUsers)  
+router.get('/:id', userController.getUserById)
+router.post('/', userController.createUser) 
+router.put('/:id', userController.updateUser)   
+router.delete('/:id', userController.deleteUser)
 
-module.exports = router;
+
+module.exports = router
